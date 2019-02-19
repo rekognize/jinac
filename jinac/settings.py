27407805287217325b3aaefb821b349847f651cb
onurmatik <omat@gezgin.com>
2019-02-19 09:49:20 +0300

    settings updates

--- jinac/settings.py
+++ jinac/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
 
-    'django_extensions',
+    #'django_extensions',
     'translations',
 
     'jinac.people.apps.PeopleConfig',
