8f54c6bc01d8378d476465aa4652e0a16d29b0c8
onurmatik <omat@gezgin.com>
2019-03-15 13:43:00 +0300

    profile path

--- jinac/settings.py
+++ jinac/settings.py
@@ -147,7 +147,7 @@ LOCALE_PATHS = (
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-#MEDIA_URL = 'http://rekognize-reports.s3.amazonaws.com/'
+#MEDIA_URL = 'http://pressinarrest.s3.amazonaws.com/'
 
 
 try:
