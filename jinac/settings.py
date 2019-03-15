a894d6dfcdf183c29f69b2d35c6689cf01bcd320
onurmatik <omat@gezgin.com>
2019-03-15 12:49:34 +0300

    settings update

--- jinac/settings.py
+++ jinac/settings.py
@@ -148,3 +148,9 @@ LOCALE_PATHS = (
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 #MEDIA_URL = 'http://rekognize-reports.s3.amazonaws.com/'
+
+
+try:
+    from .settings_local import *
+except ImportError:
+    pass
