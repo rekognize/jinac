025f8524fb5f63bac29f1cb72913423680627cdc
onurmatik <omat@gezgin.com>
2019-04-01 18:33:30 +0300

    carousel update

--- jinac/news/models.py
+++ jinac/news/models.py
@@ -4,7 +4,7 @@ from django.utils.translation import ugettext_lazy as _
 
 class Carousel(models.Model):
     image = models.ImageField(_('photo'), upload_to='carousel/')
-    text = models.CharField(_('text'), max_length=250)
+    text = models.CharField(_('text'), max_length=250, blank=True, null=True)
     link = models.URLField(_('link'), blank=True, null=True)
     added = models.DateTimeField(auto_now_add=True)
     publish = models.BooleanField(_('publish'), default=True)
