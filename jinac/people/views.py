015aacd8f64fed2c1b04d12224409bf7d80d93f1
onurmatik <omat@gezgin.com>
2019-04-04 14:08:00 +0300

    bos gazetecileri gosterme

--- jinac/people/views.py
+++ jinac/people/views.py
@@ -7,7 +7,8 @@ class JournalistListView(ListView):
 
     def get_queryset(self):
         qs = super().get_queryset()
-        qs = qs.filter(publish=True)
+        qs = qs.filter(publish=True).exclude(short_bio__isnull=True).exclude(photo__isnull=True).\
+            exclude(short_bio='').exclude(photo='')
         if self.request.GET.get('f'):
             qs = qs.filter(name__istartswith=self.request.GET.get('f'))
         return qs
