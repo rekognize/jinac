567f08a00b0d490cfe03be6661b651d71edb3586
onurmatik <omat@gezgin.com>
2019-06-14 11:46:51 +0300

    article updates

--- jinac/articles/views.py
+++ jinac/articles/views.py
@@ -8,6 +8,7 @@ class ArticleListView(ListView):
 
     def get_queryset(self):
         qs = super().get_queryset().filter(publish=True)
+        qs = qs.exclude(about_page=True)
         if self.request.LANGUAGE_CODE == 'en':
             qs = qs.filter(lang='en')
         else:
@@ -28,8 +29,12 @@ class AboutView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
+        articles = Article.objects.filter(publish=True).filter(about_page=True)
+        if self.request.LANGUAGE_CODE == 'en':
+            articles = articles.filter(lang='en')
+        else:
+            articles = articles.filter(lang='tr')
         context.update({
-            # TODO: filter by current lang
-            'article': Article.objects.filter(publish=True).filter(about_page=True).first()
+            'article': articles.first()
         })
         return context
