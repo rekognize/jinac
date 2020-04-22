c4c4c528f2bf6850d7498878e3f9e881b3b8196f
onurmatik <onurmatik@gmail.com>
2020-04-22 12:07:56 +0300

    SPK kanunu eklendi

--- jinac/cases/models.py
+++ jinac/cases/models.py
@@ -185,6 +185,7 @@ class Article(models.Model):
         (4, _('MİT')),
         (5, _('TGYK')),
         (6, _('Press Law')),
+        (7, _('SPK')),
     ))
     description = models.TextField(_('article'), blank=True, null=True)
     indictment = models.CharField(_('indictment'), max_length=200, blank=True, null=True)
